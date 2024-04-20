7251e1bb113e400e06433ced54118f57d6e0d79d
Daniel Larson <danielkeithlarson@gmail.com>
2024-04-20 08:45:36 -0500

    update build.zig

--- build.zig
+++ build.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const module = b.addModule("interface.zig", .{
-        .source_file = .{ .path = "src/interface.zig" },
+        .root_source_file = .{ .path = "src/interface.zig" },
     });
 
     const if_test = b.addTest(.{
         .root_source_file = .{ .path = "src/examples.zig" },
     });
-    if_test.addModule("interface.zig", module);
+    
+    if_test.root_module.addImport("interface.zig", module);
 
     b.getInstallStep().dependOn(&b.addRunArtifact(if_test).step);
 }
